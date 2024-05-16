d1497a11bb781b1da5d76290e8e35314e8cd35ba
mdsujan-mriddha <mdsujan134523@gmail.com>
2024-05-17 01:25:44 +0600

    issue on image

--- controller/propertyController.js
+++ controller/propertyController.js
@@ -32,13 +32,13 @@ exports.newProperty = catchAsyncsErrors(async (req, res, next) => {
     // if (!req.file) return next(new ErrorHandler("Please add a image", 400));
 
     // const file = getDataUri(req.file);
-    const file = req.file;
-    const myCloud = await cloudinary.v2.uploader.upload(file.content);
+    const image = req.file;
+    // const myCloud = await cloudinary.v2.uploader.upload(file.content);
 
-    const image = {
-        public_id: myCloud.public_id,
-        url: myCloud.secure_url
-    }
+    // const image = {
+    //     public_id: myCloud.public_id,
+    //     url: myCloud.secure_url
+    // }
 
     await Property.create({
         title,
