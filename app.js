3776d94be7eed3fee68b8981c760e3552bf7c9c0
mdsujan-mriddha <mdsujan134523@gmail.com>
2024-05-16 17:36:17 +0600

    add cors

--- app.js
+++ app.js
@@ -10,7 +10,14 @@ const dotenv = require("dotenv");
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(
+    {
+        "origin": "*",
+        "preflightContinue": false,
+        "optionsSuccessStatus": 200
+    }
+))
+app.options('*', cors()) // include before other routes
 // config 
 dotenv.config({ path: "./config/config.env" });
 // user 
